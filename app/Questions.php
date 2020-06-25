5fe4f1946781cdcacffd6d87bb23a70bbdf656da
kulvir kaur <kulvirkaur@kulvirs-MacBook-Air.local>
2020-06-24 23:29:02 -0400

    updates in question module

--- app/Questions.php
+++ app/Questions.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Questions extends Model
 {
-    //
+    //for mass assignments
+    protected $fillable = ["title", "body"];
+
+    //relationship with user table
+    public function users() {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * relationship with Question model
+     */
+    public function questions() {
+        return $this->hasMany(Questions::class);
+    }
+
+    /**
+     * add slug 
+     */
+    public function setTitleAttribute($value) {
+        $this->title = $value;
+        $this->slug = Str::slug($value);
+    }
 }
